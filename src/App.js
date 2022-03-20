93d818671031b1bfdadda27a7b04d2e35d0d82b1
bloomrooms <bloomrooms@bloomroomss-iMac.local>
2022-03-20 12:51:38 +0530

    Added Preloader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, { useEffect, useState } from 'react';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import styled from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
+import Lottie from 'react-lottie';
+import * as animationData from './assets/lotties/preloader.json';
+
 import {
 	Home,
 	AboutPage,
@@ -12,21 +16,52 @@ import {
 } from './pages';
 
 function App() {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 6000);
+	}, []);
+
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData,
+		rendererSettings: {
+			preserveAspectRatio: 'xMidYMid slice',
+		},
+	};
+
 	return (
-		<AppContainer>
-			<Navbar />
-			<MainContainer>
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/about' element={<AboutPage />} />
-					<Route path='/travel' element={<TravelPage />} />
-					<Route path='/hospitality' element={<HospitalityPage />} />
-					<Route path='/contact' element={<ContactPage />} />
-					<Route path='*' element={<ErrorPage />} />
-				</Routes>
-			</MainContainer>
-			<Footer />
-		</AppContainer>
+		<>
+			{loading ? (
+				<Lottie
+					options={defaultOptions}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						minHeight: '100vh',
+					}}
+					height={400}
+					width={400}
+				/>
+			) : (
+				<AppContainer>
+					<Navbar />
+					<MainContainer>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/about' element={<AboutPage />} />
+							<Route path='/travel' element={<TravelPage />} />
+							<Route path='/hospitality' element={<HospitalityPage />} />
+							<Route path='/contact' element={<ContactPage />} />
+							<Route path='*' element={<ErrorPage />} />
+						</Routes>
+					</MainContainer>
+					<Footer />
+				</AppContainer>
+			)}
+		</>
 	);
 }
 
